24e5395dd5461ef0b9087a5801cd1d830121e195
ekelly <eric.k.125@gmail.com>
2015-01-14 15:13:38 -0500

    Average (Rust)

---
+++ average/src/main.rs
@@ -0,0 +1,67 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    let mut stdin = io::stdin();
+    let mut lock = stdin.lock();
+    let mut lines = lock.lines();
+    let mut data = vec![];
+    for line in lines {
+        let l = line.unwrap();
+        let trimmed = l.trim();
+        if trimmed == "999" {
+            break;
+        } else {
+            match trimmed.parse::<f64>() {
+                Some(x) => data.push(x),
+                None    => {
+                    continue;
+                }
+            }
+        }
+    }
+
+    data.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+    let avg = average(&data);
+
+    println!("Average: {}", avg.unwrap());
+}
+
+// Averages the array of floats
+fn average(data: &Vec<f64>) -> Option<f64> {
+    if data.len() == 0 {
+        return None;
+    }
+    let mut count = 0f64;
+    let mut sum = 0f64;
+    for &d in data.iter() {
+        sum = sum + d;
+        count = count + 1.0;
+    }
+    Option::Some(sum / count)
+}
+
+#[cfg(test)]
+mod AverageTests {
+    use super::average;
+
+    #[test]
+    fn test_average() {
+        let v1: Vec<f64> = vec![1f64, 2f64, 3f64];
+        assert_eq!(2, average(&v1));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
