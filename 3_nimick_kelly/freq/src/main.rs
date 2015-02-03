34802407c50461ba74bc376c542ce3474c8134a9
Frnacis Nimick <fnimick@gmail.com>
2015-02-02 20:13:20 -0500

    skeleton of inc_count and tests

--- 3_nimick_kelly/freq/src/main.rs
+++ 3_nimick_kelly/freq/src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn inc_count(map: &mut HashMap<&str, usize>) {
+}
+
+
+#[cfg(test)]
+mod freq_tests {
+    use super::{inc_count};
+
+    #[test]
+    fn test_inc_count() {
+        let mut map = HashMap::new::<&str, usize>();
+        inc_count(&mut map, &("test"));
+        inc_count(&mut map, &("test"));
+        inc_count(&mut map, &("one"));
+        !assert(!map.contains_key(&("nope")));
+        !assert_eq(map.get(&("test")).unwrap(), 2);
+        !assert_eq(map.get(&("one")).unwrap(), 1);
+    }
 }
