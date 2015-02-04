4afdcad29a485b5e95f0ce7b0b63cde208b9fec0
ekelly <eric.k.125@gmail.com>
2015-02-03 23:44:21 -0500

    Fixed bug

--- 3_nimick_kelly/freq/src/main.rs
+++ 3_nimick_kelly/freq/src/main.rs
@@ -16,12 +16,12 @@ fn main() {
     let stdin: BufferedReader<StdinReader> = BufferedReader::new(io::stdin());
 }
 
-fn parse_lines<'a, R: Reader>(mut reader: BufferedReader<R>) -> HashMap<&'a str, usize> {
-    let mut wordcounts: HashMap<&str, usize> = HashMap::new();
+fn parse_lines<R: Reader>(mut reader: BufferedReader<R>) -> HashMap<String, usize> {
+    let mut wordcounts: HashMap<String, usize> = HashMap::new();
     for line in reader.lines() {
         let l = line.unwrap();
         for word in l.words() {
-            inc_count(&mut wordcounts, word.clone());
+            inc_count(&mut wordcounts, String::from_str(word));
         }
     }
     wordcounts
@@ -30,33 +30,49 @@ fn parse_lines<'a, R: Reader>(mut reader: BufferedReader<R>) -> HashMap<&'a str,
 #[cfg(test)]
 mod parse_lines_tests {
     use super::{parse_lines};
+    use std::collections::HashMap;
+    use std::io::{MemReader,BufferedReader};
 
     #[test]
     fn tests() {
-        let mut expected: HashMap<&str, usize> = HashMap::new();
-        expected.insert("Hello", 1);
-        expected.insert("World", 2);
-        expected.insert("Today", 1);
-        expected.insert("is", 1);
-        expected.insert("the", 2);
-        expected.insert("best", 1);
-        expected.insert("day", 1);
-        expected.insert("in", 1);
+        let mut expected: HashMap<String, usize> = HashMap::new();
+        expected.insert(String::from_str("Hello,"), 1);
+        expected.insert(String::from_str("World!"), 2);
+        expected.insert(String::from_str("Today"), 1);
+        expected.insert(String::from_str("is"), 1);
+        expected.insert(String::from_str("the"), 2);
+        expected.insert(String::from_str("best"), 1);
+        expected.insert(String::from_str("day"), 1);
+        expected.insert(String::from_str("in"), 1);
         parse_lines_expect("Hello, World!\nToday is the best day in the World!",
                            expected);
     }
 
-    fn parse_lines_expect(input: &str, expected: HashMap<&str, usize>) {
+    fn parse_lines_expect(input: &str, expected: HashMap<String, usize>) {
         let bytes = input.to_string().into_bytes();
         let r: BufferedReader<MemReader> =
             BufferedReader::new(MemReader::new(bytes));
-        let output = parse_lines(r);
-        assert_eq!(expected, output);
+        let mut output = parse_lines(r);
+        let mut found_keys = Vec::new();
+        for (word, count) in output.iter_mut() {
+            assert!(expected.contains_key(word));
+            match expected.get(word) {
+                    Some(expected_count) => assert_eq!(count, expected_count),
+                    None                 => assert!(false)
+            }
+            found_keys.push(word);
+        }
+        /*
+        for key in found_keys.iter() {
+            output.remove(key);
+        }
+        assert_eq!(output.len(), 0);
+        */
     }
 }
 
-fn inc_count<'a>(map: &mut HashMap<&'a str, usize>, word: &'a str) {
-    match map.get_mut(word) {
+fn inc_count(map: &mut HashMap<String, usize>, word: String) {
+    match map.get_mut(&word) {
         Some(count) => {*count += 1; return;},
         None => {},
     }
@@ -71,11 +87,11 @@ mod inc_count_tests {
     #[test]
     fn test_inc_count() {
         let mut map = HashMap::new();
-        inc_count(&mut map, "test");
-        inc_count(&mut map, "test");
-        inc_count(&mut map, "one");
-        assert!(!map.contains_key("nope"));
-        assert_eq!(*map.get("test").unwrap(), 2);
-        assert_eq!(*map.get("one").unwrap(), 1);
+        inc_count(&mut map, String::from_str("test"));
+        inc_count(&mut map, String::from_str("test"));
+        inc_count(&mut map, String::from_str("one"));
+        assert!(!map.contains_key(&String::from_str("nope")));
+        assert_eq!(*map.get(& String::from_str("test")).unwrap(), 2);
+        assert_eq!(*map.get(& String::from_str("one")).unwrap(), 1);
     }
 }
