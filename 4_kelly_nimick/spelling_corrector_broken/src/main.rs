c4835e6f8e5b30144ae4301e2b049cafb178eb9d
ekelly <eric.k.125@gmail.com>
2015-02-17 18:41:19 -0500

    Isolated panic

--- 4_kelly_nimick/spelling_corrector_broken/src/main.rs
+++ 4_kelly_nimick/spelling_corrector_broken/src/main.rs
@@ -1,25 +1,8 @@
-#![allow(unstable)]
-use std::collections::HashMap;
-use std::io::BufferedReader;
-use std::os;
-
 fn main() {
-    let mut args = os::args();
-    let train = match args.iter().skip(1).take(1).next() {
-        Some(file) => file.as_slice(),
-        None       => panic!("Must provide training file")
-    };
-    let file_reader = open_file("hello");
-    let dictionary = train(file_reader);
+    let train = "foo";
+    let x = train("hello");
 }
 
-fn open_file<T>(filename: &str) -> BufferedReader<T> {
-    use std::io::File;
-
-    let file = File::open(&Path::new(filename));
-    BufferedReader::new(file)
-}
-
-fn train<T>(file: BufferedReader<T>) -> HashMap<String, usize> {
-    HashMap::new();
+fn train(file: &str) -> bool {
+    true
 }
