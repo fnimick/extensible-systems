f0aebc18d9979ca5338fa024c87b86f559ec3f6b
Francis Nimick <fnimick@gmail.com>
2015-03-08 22:23:00 -0400

    server hello world

--- 6_nimick/web_server/src/rustyd.rs
+++ 6_nimick/web_server/src/rustyd.rs
@@ -6,7 +6,25 @@ use std::io::net::tcp;
 static BIND_ADDR: &'static str = "127.0.0.1:8000";
 static INDEX_FILES: [&'static str; 3] = ["index.html", "index.shtml", "index.txt"];
 
+pub fn handle_client(mut stream: TcpStream) {
+    let response = b"Hello world.";
+    match stream.write(response) {
+        Ok(()) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
+
 pub fn serve_forever() {
-    loop {
+    let listener = TcpListener::bind(BIND_ADDR).unwrap();
+    let mut acceptor = listener.listen().unwrap();
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => {},
+            Ok(stream) => {
+                Thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+        }
     }
 }
