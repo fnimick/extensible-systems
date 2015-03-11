f749d50242a73762166f8a80c41d52b725ce8237
ekelly <eric.k.125@gmail.com>
2015-03-11 16:44:54 -0400

    Changed String borrows to str borrows

--- 6_nimick/web_server/src/files.rs
+++ 6_nimick/web_server/src/files.rs
@@ -24,21 +24,21 @@ impl FileResult {
 
 /// If we find PermissionDenied or FileError as the result of opening an index
 /// file, then that is returned.
-pub fn open_file_with_indices(path: &String) -> (FileResult, bool) {
+pub fn open_file_with_indices(path: &str) -> (FileResult, bool) {
     if !path.is_empty() && path.chars().rev().next().unwrap() != '/' {
-        return (open_file(path), is_html(path));
+        return (open_file(path.as_slice()), is_html(path.as_slice()));
     }
     for index_file in INDEX_FILES.iter() {
         let index_path = path.clone().to_string() + *index_file;
-        match open_file(&index_path) {
+        match open_file(index_path.as_slice()) {
             NotFound => continue,
-            r => return (r, is_html(&index_path))
+            r => return (r, is_html(index_path.as_slice()))
         }
     }
     (NotFound, false)
 }
 
-pub fn open_file(path: &String) -> FileResult {
+pub fn open_file(path: &str) -> FileResult {
     match File::open(&Path::new(path)) {
         Ok(f) => FileOk(BufferedReader::new(f)),
         Err(IoError{kind:IoErrorKind::FileNotFound, ..}) => NotFound,
@@ -47,15 +47,15 @@ pub fn open_file(path: &String) -> FileResult {
     }
 }
 
-fn is_html(s: &String) -> bool {
+fn is_html(s: &str) -> bool {
     s.split('.').rev().next().unwrap() == "html"
 }
 
 
 #[cfg(test)]
 mod OpenFileTests {
-    use super::open_file;
-    use super::FileResult;
+    use super::{FileResult, open_file};
+
     #[test]
     fn test_file_not_exist() {
         let my_str = "wharrgarbl".to_string();
@@ -63,5 +63,9 @@ mod OpenFileTests {
             FileResult::NotFound => (),
             _ => panic!("bang"),
         }
+    }
+
+    #[test]
+    fn test_file_exists() {
     }
 }
