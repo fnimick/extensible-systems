fa861ec23c21f5de59bd3b7369fe67aff397bf12
ekelly <eric.k.125@gmail.com>
2015-03-11 17:03:30 -0400

    Fixed String and str

--- 6_nimick/web_server/src/files.rs
+++ 6_nimick/web_server/src/files.rs
@@ -26,19 +26,19 @@ impl FileResult {
 /// file, then that is returned.
 pub fn open_file_with_indices(path: &String) -> (FileResult, bool) {
     if !path.is_empty() && path.chars().rev().next().unwrap() != '/' {
-        return (open_file(path), is_html(path));
+        return (open_file(path.as_slice()), is_html(path.as_slice()));
     }
     for index_file in INDEX_FILES.iter() {
         let index_path = path.clone() + *index_file;
-        match open_file(&index_path) {
+        match open_file(index_path.as_slice()) {
             NotFound => continue,
-            r => return (r, is_html(&index_path))
+            r => return (r, is_html(index_path.as_slice()))
         }
     }
     (NotFound, false)
 }
 
-pub fn open_file(path: &String) -> FileResult {
+pub fn open_file(path: &str) -> FileResult {
     match File::open(&Path::new(path)) {
         Ok(f) => FileOk(BufferedReader::new(f)),
         Err(IoError{kind:IoErrorKind::FileNotFound, ..}) => NotFound,
@@ -47,7 +47,7 @@ pub fn open_file(path: &String) -> FileResult {
     }
 }
 
-fn is_html(s: &String) -> bool {
+fn is_html(s: &str) -> bool {
     s.split('.').rev().next().unwrap_or("") == "html"
 }
 
@@ -58,8 +58,8 @@ mod OpenFileTests {
 
     #[test]
     fn test_file_not_exist() {
-        let my_str = "wharrgarbl".to_string();
-        match open_file(&my_str) {
+        let my_str = "wharrgarbl";
+        match open_file(my_str) {
             FileResult::NotFound => (),
             _ => panic!("bang"),
         }
