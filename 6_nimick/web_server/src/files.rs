04028d51e93fdd7aa970bc33a30fa30f987e4a31
ekelly <eric.k.125@gmail.com>
2015-03-11 18:08:12 -0400

    Added tests

--- 6_nimick/web_server/src/files.rs
+++ 6_nimick/web_server/src/files.rs
@@ -12,6 +12,7 @@ pub enum FileResult {
 
 impl FileResult {
 
+    /// Return the HTTP message and code associated with the FileResult
     pub fn as_str(&self) -> &str {
         match *self {
             FileOk(..) => "200 OK",
@@ -24,6 +25,8 @@ impl FileResult {
 
 /// If we find PermissionDenied or FileError as the result of opening an index
 /// file, then that is returned.
+/// A borrowed String is passed in rather than a &str, because we are
+/// modifying its contents
 pub fn open_file_with_indices(path: &String) -> (FileResult, bool) {
     if !path.is_empty() && path.chars().rev().next().unwrap() != '/' {
         return (open_file(path.as_slice()), is_html(path.as_slice()));
@@ -38,6 +41,39 @@ pub fn open_file_with_indices(path: &String) -> (FileResult, bool) {
     (NotFound, false)
 }
 
+#[cfg(test)]
+mod open_file_with_indices_tests {
+    use super::{FileResult, open_file_with_indices};
+
+    #[test]
+    fn test_file_not_exist() {
+        let my_str = "wharrgarbl".to_string();
+        match open_file_with_indices(&my_str) {
+            (FileResult::NotFound, false) => (),
+            _ => panic!("bang"),
+        }
+    }
+
+    #[test]
+    fn test_file_exists() {
+        let my_str = "test/index.html".to_string();
+        match open_file_with_indices(&my_str) {
+            (FileResult::FileOk(..), true) => (),
+            _ => panic!("bang"),
+        }
+    }
+
+    #[test]
+    fn test_directory() {
+        let my_str = "test/".to_string();
+        match open_file_with_indices(&my_str) {
+            (FileResult::FileOk(..), true) => (),
+            _ => panic!("bang"),
+        }
+    }
+}
+
+/// Open the file at the path given by the input &str
 pub fn open_file(path: &str) -> FileResult {
     match File::open(&Path::new(path)) {
         Ok(f) => FileOk(BufferedReader::new(f)),
@@ -47,13 +83,8 @@ pub fn open_file(path: &str) -> FileResult {
     }
 }
 
-fn is_html(s: &str) -> bool {
-    s.split('.').rev().next().unwrap_or("") == "html"
-}
-
-
 #[cfg(test)]
-mod OpenFileTests {
+mod open_file_tests {
     use super::{FileResult, open_file};
 
     #[test]
@@ -67,5 +98,27 @@ mod OpenFileTests {
 
     #[test]
     fn test_file_exists() {
+        let my_str = "test/index.html";
+        match open_file(my_str) {
+            FileResult::FileOk(..) => (),
+            _ => panic!("bang"),
+        }
+    }
+}
+
+/// Determine if the file ends with html
+fn is_html(s: &str) -> bool {
+    s.split('.').rev().next().unwrap_or("") == "html"
+}
+
+#[cfg(test)]
+mod is_html_tests {
+    use super::is_html;
+
+    #[test]
+    fn test_is_html() {
+        assert!(is_html("foo/bar/test.html"));
+        assert!(!is_html("foo/bar/test.xhtml"));
+        assert!(!is_html("!/foo/html/test"));
     }
 }
